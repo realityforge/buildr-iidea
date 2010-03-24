a0dcbb8e54e1bff50a5a8ce6c7d6227a1d34495b
Rhett Sutphin <rhett@detailedbalance.net>
2010-03-23 21:19:46 -0500

    Initial, not-nearly-adequate spec suite

--- spec/buildr/iidea_spec.rb
+++ spec/buildr/iidea_spec.rb
@@ -1,7 +1,52 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require File.expand_path('../../spec_helper', __FILE__)
 
-describe "BuildrIidea" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+# TODO: these specs are laughably incomplete
+
+describe "iidea" do
+  it "provides an 'iidea' task'" do
+    Rake::Task.tasks.collect(&:to_s).should include("iidea")
+  end
+
+  it "provides an 'iidea:clean' task'" do
+    Rake::Task.tasks.collect(&:to_s).should include("iidea:clean")
+  end
+
+  describe "documentation" do
+    before do
+      define 'any'
+    end
+
+    it "describes iidea" do
+      task('iidea').comment.should == "Generate Intellij IDEA artifacts for all projects"
+    end
+
+    it "describes iidea:clean" do
+      task('iidea:clean').comment.should == "Delete the generated Intellij IDEA artifacts"
+    end
+  end
+
+  describe "IPR generation" do
+    before do
+      mkdir_p 'bar'
+      mkdir_p 'baz'
+
+      @foo = define "foo" do
+        define 'bar'
+        define 'baz'
+      end
+      task('iidea').invoke
+    end
+
+    it "generates an IPR at the top level" do
+      File.exist?(@foo._("foo-iidea.ipr")).should be_true
+    end
+
+    it "only generates one IPR" do
+      Dir[@foo._("**/*.ipr")].should have(1).entry
+    end
+
+    it "informs the user about what it's doing" do
+      $messages[:info].should include("Writing #{@foo._('foo-iidea.ipr')}")
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,20 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'buildr-iidea'
-require 'spec'
-require 'spec/autorun'
+begin
+  require File.expand_path("../../.bundle/environment", __FILE__)
+rescue LoadError
+  raise "Gem environment is not prepared.  Run `bundle install` before running specs."
+end
 
-Spec::Runner.configure do |config|
-  
+require 'spec'
+
+# hook into buildr's spec_helpers load process
+unless defined?(SpecHelpers)
+  module SandboxHook
+    def SandboxHook.included(spec_helpers)
+      $LOAD_PATH.unshift(File.dirname(__FILE__))
+      $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+      require 'buildr/iidea'
+    end
+  end
+
+  require File.expand_path('../../vendor/buildr/spec/spec_helpers', __FILE__)
 end
