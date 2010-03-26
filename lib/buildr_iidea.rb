64bd481a658282b0adab0de3f653a572d6528afa
Peter Donald <peter@realityforge.org>
2010-03-26 21:12:17 +1100

    Move the module tha is included in the Buildr project into a file named project_extension

--- lib/buildr_iidea.rb
+++ lib/buildr_iidea.rb
@@ -2,4 +2,4 @@ require 'buildr'
 require File.expand_path(File.dirname(__FILE__) + '/buildr/intellij_idea/idea_file')
 require File.expand_path(File.dirname(__FILE__) + '/buildr/intellij_idea/idea_module')
 require File.expand_path(File.dirname(__FILE__) + '/buildr/intellij_idea/idea_project')
-require File.expand_path(File.dirname(__FILE__) + '/buildr/iidea')
+require File.expand_path(File.dirname(__FILE__) + '/buildr/intellij_idea/project_extension')
