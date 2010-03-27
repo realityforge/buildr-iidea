a72ee6f7e9d0585ee30e055a5de275f8b4dc9bd9
Peter Donald <peter@realityforge.org>
2010-03-27 14:49:14 +1100

    Customize spec helper to add some iidea specific helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,4 +17,19 @@ unless defined?(SpecHelpers)
   end
 
   require File.expand_path('../../vendor/buildr/spec/spec_helpers', __FILE__)
+
+  module SpecHelpers
+    def root_project_filename(project_name)
+      "#{project_name}#{Buildr::IntellijIdea::IdeaFile::DEFAULT_SUFFIX}.ipr"
+    end
+
+    def root_module_filename(project_name)
+      "#{project_name}#{Buildr::IntellijIdea::IdeaFile::DEFAULT_SUFFIX}.iml"
+    end
+
+    def subproject_module_filename(parent_project_name, project_name)
+      "#{project_name}/#{parent_project_name}-#{project_name}#{Buildr::IntellijIdea::IdeaFile::DEFAULT_SUFFIX}.iml"
+    end
+  end
+
 end
