60f0ad3873ef8c7c8a2d4f546c94718d1b31d2e4
Peter Donald <peter@realityforge.org>
2010-03-27 14:49:39 +1100

    Add spec to check that the module and project can have different suffixes

--- spec/buildr/intellij_idea/suffix_spec.rb
+++ spec/buildr/intellij_idea/suffix_spec.rb
@@ -4,25 +4,22 @@ describe "iidea" do
   describe "Artifact generation with default suffix" do
     before do
       mkdir_p 'bar'
-      mkdir_p 'baz'
 
       @foo = define "foo" do
         define 'bar'
-        define 'baz'
       end
       task('iidea').invoke
     end
 
     it "generates an IPR at the top level" do
-      File.should be_exist(@foo._("foo-iidea.ipr"))
+      File.should be_exist(@foo._(root_project_filename("foo")))
     end
 
     it  "generates an IPR with correct module references" do
-      module_file = @foo._("foo-iidea.ipr")
+      module_file = @foo._(root_project_filename("foo"))
       results = File.read(module_file)
       results.should =~ /file:\/\/\$PROJECT_DIR\$\/foo-iidea\.iml/
       results.should =~ /file:\/\/\$PROJECT_DIR\$\/bar\/foo-bar-iidea\.iml/
-      results.should =~ /file:\/\/\$PROJECT_DIR\$\/baz\/foo-baz-iidea\.iml/
     end
 
     it "generates an IML at the top level" do
@@ -31,20 +28,17 @@ describe "iidea" do
 
     it "generates an IML in subprojects" do
       File.should be_exist(@foo._("bar/foo-bar-iidea.iml"))
-      File.should be_exist(@foo._("baz/foo-baz-iidea.iml"))
     end
   end
 
   describe "IPR generation with empty suffix" do
     before do
       mkdir_p 'bar'
-      mkdir_p 'baz'
 
       @foo = define "foo" do
         ipr.suffix = ''
         iml.suffix = ''
         define 'bar'
-        define 'baz'
       end
       task('iidea').invoke
     end
@@ -58,7 +52,6 @@ describe "iidea" do
       results = File.read(module_file)
       results.should =~ /file:\/\/\$PROJECT_DIR\$\/foo\.iml/
       results.should =~ /file:\/\/\$PROJECT_DIR\$\/bar\/foo-bar\.iml/
-      results.should =~ /file:\/\/\$PROJECT_DIR\$\/baz\/foo-baz\.iml/
     end
 
     it "generates an IML at the top level" do
@@ -67,8 +60,41 @@ describe "iidea" do
 
     it "generates an IML in subprojects" do
       File.should be_exist(@foo._("bar/foo-bar.iml"))
-      File.should be_exist(@foo._("baz/foo-baz.iml"))
     end
 
   end
+
+  describe "IPR generation with specific suffix" do
+    before do
+      mkdir_p 'bar'
+
+      @foo = define "foo" do
+        ipr.suffix = '-X'
+        iml.suffix = '-Y'
+        define 'bar'
+      end
+      task('iidea').invoke
+    end
+
+    it "generates an IPR at the top level" do
+      File.should be_exist(@foo._("foo-X.ipr"))
+    end
+
+    it  "generates an IPR with correct module references" do
+      module_file = @foo._("foo-X.ipr")
+      results = File.read(module_file)
+      results.should =~ /file:\/\/\$PROJECT_DIR\$\/foo-Y\.iml/
+      results.should =~ /file:\/\/\$PROJECT_DIR\$\/bar\/foo-bar-Y\.iml/
+    end
+
+    it "generates an IML at the top level" do
+      File.should be_exist(@foo._("foo-Y.iml"))
+    end
+
+    it "generates an IML in subprojects" do
+      File.should be_exist(@foo._("bar/foo-bar-Y.iml"))
+    end
+
+  end
+
 end
