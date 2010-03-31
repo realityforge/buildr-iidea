48b0384120376fd49291978dffbf370995154250
Peter Donald <peter@realityforge.org>
2010-03-31 22:01:02 +1100

    Remove duplicated tests

--- spec/buildr/iidea_spec.rb
+++ spec/buildr/iidea_spec.rb
@@ -3,28 +3,6 @@ require File.expand_path('../../spec_helper', __FILE__)
 # TODO: these specs are laughably incomplete
 
 describe "iidea" do
-  it "provides an 'iidea' task'" do
-    Rake::Task.tasks.collect(&:to_s).should include("iidea")
-  end
-
-  it "provides an 'iidea:clean' task'" do
-    Rake::Task.tasks.collect(&:to_s).should include("iidea:clean")
-  end
-
-  describe "documentation" do
-    before do
-      define 'any'
-    end
-
-    it "describes iidea" do
-      task('iidea').comment.should == "Generate Intellij IDEA artifacts for all projects"
-    end
-
-    it "describes iidea:clean" do
-      task('iidea:clean').comment.should == "Delete the generated Intellij IDEA artifacts"
-    end
-  end
-
   describe "IPR generation" do
     before do
       mkdir_p 'bar'
