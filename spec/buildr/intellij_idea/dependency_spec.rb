edbc864ddb4e0d4ae26dc8b7eb3d81ceff0d0e1a
Peter Donald <peter@realityforge.org>
2010-03-31 22:41:09 +1100

    Rewrite the dependency spec to be xml based

--- spec/buildr/intellij_idea/dependency_spec.rb
+++ spec/buildr/intellij_idea/dependency_spec.rb
@@ -1,61 +1,86 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
-describe "iidea" do
-  describe "IdeaModule.local_repository_env_override" do
-    describe "uses M2_REPO by default" do
-      before do
-        artifact('group:id:jar:1.0') { |t| write t.to_s }
+ORDER_ENTRY_XPATH = "/module/component[@name='NewModuleRootManager']/orderEntry"
+DEPENDENCY_NAME = 'group:id:jar:1.0'
+DEPENDENCY2_NAME = 'group:id2:jar:1.0'
 
+describe "generate task" do
+
+  describe "with a single dependency" do
+    before do
+      @artifact = artifact(DEPENDENCY_NAME) { |t| write t.to_s }
+    end
+
+    describe "of type compile" do
+      before do
         @foo = define "foo" do
-          compile.with 'group:id:jar:1.0'
+          compile.with DEPENDENCY_NAME
         end
         task('iidea').invoke
       end
 
-      it "generates IML with a dependency" do
-        module_file = root_module_filename(@foo)
-        File.should be_exist(module_file)
-        File.read(module_file).should =~ /jar:\/\/\$M2_REPO\$\/group\/id\/1\.0\/id-1\.0\.jar\!\//
+      it "generates one exported 'module-library' orderEntry in IML" do
+        root_module_xml(@foo).should have_nodes("#{ORDER_ENTRY_XPATH}[@type='module-library', @exported='']", 1)
       end
     end
 
-    describe "uses absolute paths when nil specified" do
+    describe "of type test" do
       before do
-        artifact('group:id:jar:1.0') { |t| write t.to_s }
+        @foo = define "foo" do
+          test.with DEPENDENCY_NAME
+        end
+        task('iidea').invoke
+      end
 
+      it "generates one non-exported 'module-library' orderEntry in IML" do
+        pending
+        root_module_xml(@foo).should have_nodes("#{ORDER_ENTRY_XPATH}[@type='module-library', @exported='']", 0)
+        root_module_xml(@foo).should have_nodes("#{ORDER_ENTRY_XPATH}[@type='module-library']", 1)
+      end
+    end
+
+    describe "with local_repository_env_override set to nil" do
+      before do
         @foo = define "foo" do
           iml.local_repository_env_override = nil
-          compile.with 'group:id:jar:1.0'
+          compile.with DEPENDENCY_NAME
         end
         task('iidea').invoke
       end
 
-      it "generates IML with a dependency" do
-        module_file = root_module_filename(@foo)
-        File.should be_exist(module_file)
-        File.read(module_file).should_not =~ /\$M2_REPO\$/
+      it "generates orderEntry with absolute path for classes jar" do
+        root_module_xml(@foo).should match_xpath("#{ORDER_ENTRY_XPATH}/library/CLASSES/root/@url",
+                                                 "jar://#{@artifact.to_s}!/")
       end
     end
-
-
-    describe "uses MAVEN_REPOSITORY in paths when specified" do
+    describe "with local_repository_env_override set to MAVEN_REPOSITORY" do
       before do
-        artifact('group:id:jar:1.0') { |t| write t.to_s }
-
         @foo = define "foo" do
-          iml.local_repository_env_override = "MAVEN_REPOSITORY"
-          compile.with 'group:id:jar:1.0'
+          iml.local_repository_env_override = 'MAVEN_REPOSITORY'
+          compile.with DEPENDENCY_NAME
         end
         task('iidea').invoke
       end
 
-      it "generates IML with a dependency" do
-        module_file = root_module_filename(@foo)
-        File.should be_exist(module_file)
-        File.read(module_file).should =~ /\$MAVEN_REPOSITORY\$/
-        File.read(module_file).should_not =~ /\$M2_REPO\$/
+      it "generates orderEntry with absolute path for classes jar" do
+        root_module_xml(@foo).should match_xpath("#{ORDER_ENTRY_XPATH}/library/CLASSES/root/@url",
+                                                 "jar://$MAVEN_REPOSITORY$/group/id/1.0/id-1.0.jar!/")
       end
     end
+  end
 
+  describe "with multiple dependencies" do
+    before do
+      @artifact1 = artifact(DEPENDENCY_NAME) { |t| write t.to_s }
+      @artifact2 = artifact(DEPENDENCY2_NAME) { |t| write t.to_s }
+      @foo = define "foo" do
+        compile.with DEPENDENCY_NAME, DEPENDENCY2_NAME
+      end
+      task('iidea').invoke
+    end
+
+    it "generates multiple 'module-library' orderEntry in IML" do
+      root_module_xml(@foo).should have_nodes("#{ORDER_ENTRY_XPATH}[@type='module-library']", 2)
+    end
   end
 end
